addf2dd67b3825ae3bca35b3d797386d0c4340ee
BehzadJanNesar <behzadjannesar.2001@gmail.com>
2024-07-07 17:46:42 +0330

    continue to create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/HomePage";
+import AppLayout from "./pages/AppLayout";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-   return <div>World wise</div>;
+   return (
+      <div>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<Homepage />} />
+               <Route path="/app" element={<AppLayout />} />
+               <Route path="/product" element={<Product />} />
+               <Route path="/pricing" element={<Pricing />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="*" element={<PageNotFound />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
 
 export default App;
